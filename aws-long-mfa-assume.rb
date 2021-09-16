769824ba13d9466b8e98fc88708740cecd68691c
asyncant <git@asyncant.com>
2021-09-16 23:28:18 +0200

    Update to aws-long-mfa-assume v0.9.0.

--- aws-long-mfa-assume.rb
+++ aws-long-mfa-assume.rb
@@ -1,9 +1,9 @@
 class AwsLongMfaAssume < Formula
   desc "Assume mfa-protected IAM roles longer than 1 hour with the aws-cli"
   homepage "https://github.com/asyncant/aws-long-mfa-assume"
-  url "https://github.com/asyncant/aws-long-mfa-assume/archive/v0.8.1.zip"
-  version "0.8.1"
-  sha256 "58dc7ff2894c4b3923e94ddf86f71a5773c5d07f634dd504ce2b8026fd66cbf2"
+  url "https://github.com/asyncant/aws-long-mfa-assume/archive/v0.9.0.zip"
+  version "0.9.0"
+  sha256 "452d287e9086dde4e8c5b88907526f3420376fe6f28136c9a6de6c189065f71c"
 
   depends_on "awscli"
   depends_on "jq"
